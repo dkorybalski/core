6ba2da7622741f71d323dd6523e879018d1a229a
Dawid Gorkiewicz <gorkiewicz.dawid@gmail.com>
2023-12-11 21:35:43 +0000

    feat: SYSPRI-268 - GET aggregated supervisor availability API

--- defense-schedule/src/main/java/pl/edu/amu/wmi/service/supervisordefense/SupervisorDefenseAssignmentService.java
+++ defense-schedule/src/main/java/pl/edu/amu/wmi/service/supervisordefense/SupervisorDefenseAssignmentService.java
@@ -1,7 +1,12 @@
 package pl.edu.amu.wmi.service.supervisordefense;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SupervisorDefenseAssignmentService {
 
     void createSupervisorDefenseAssignments(String studyYear, Long defenseScheduleConfigId);
+
+    List<LocalDate> getAllDefenseAssignmentDaysForStudyYear(String studyYear);
 
 }
